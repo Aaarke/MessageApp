97356721289409c60be6823f709d5e1c70c3a25b
ROSHAN KUMAR <roshankumar@ROSHANs-MBP.domain.name>
2021-11-27 00:39:19 +0530

    navigation support added

--- app/src/main/java/com/example/messageapp/ui/viewmodel/AllMessageViewModel.kt
+++ app/src/main/java/com/example/messageapp/ui/viewmodel/AllMessageViewModel.kt
@@ -3,8 +3,9 @@ package com.example.messageapp.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
 
 @HiltViewModel
-class AllMessageViewModel : ViewModel() {
+class AllMessageViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
